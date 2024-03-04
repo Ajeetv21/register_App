4fc6c02d2c20aa7ba20a0ea533007f6d3adc73c6
Ajeetv21 <ajeetv628@gmail.com>
2024-03-04 21:09:51 +0530

    props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,18 @@
-import React, { useState } from "react";
-import Create from "./components/Create"
-import Show from "./components/Show"
-import Home from "./components/Home"
-import Nav from "./components/Nav";
-import Details from "./components/Details";
-import { Route, Routes } from "react-router";
+import React, { useState } from 'react'
+import Create from './components/Create';
+import Show from './components/Show';
 
 const App = () => {
- 
-    const [users, setusers] = useState([
-        
-    ]);
-    const [username, setusername] = useState("");
-    const [password, setpassword] = useState("");
-
-    const SubmitHandler = (e) => {
-        e.preventDefault();
-        const user = { username, password };
-
-        const copyusers = [...users];
-        copyusers.push(user);
-        setusers(copyusers);
-
-        // setusers([...users, user])
-
-        setusername("");
-        setpassword("");
-    };
-  
-    return (
-    <>
-        <Nav/>
-        <Routes>
-            <Route path="" element={<Home/>}/>
-            <Route path="create" element={<Create users={users} setusers=
-            {setusers} />}/>
-            <Route path="/show" element={<Show users={users} setusers={setusers}/>}/>
-            <Route path="/show/details/:title" element={<Details/>}/>
-        
-        </Routes>
-       
-    </>
+    const [users, setusers] = useState(
+        JSON.parse(localStorage.getItem("users")) || []
     );
-};
+    
+  return (
+    <div>
+      <Create users={users} setusers={setusers} />
+            <Show users={users} setusers={setusers} /> 
+    </div>
+  )
+}
 
-export default App;
+export default App
--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,53 +1,45 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
-import { toast } from 'react-toastify'
+import React from "react";
+import { toast } from "react-toastify";
 
-function Show(props) {
-   const {users, setusers} = props;
-   const DeleteHandler = (index) =>{
-    const copyusers = [...users];
-    copyusers.splice(index,1);
-    setusers(copyusers);
-    localStorage.setItem("users",JSON.stringify(copyusers));
-    toast.warn("User deleted successfully !");
+const Show = (props) => {
+    const { users, setusers } = props;
+    const DeleteHandler = (index) => {
+        const copyusers = [...users];
+        copyusers.splice(index, 1);
+        setusers(copyusers);
+        localStorage.setItem("users", JSON.stringify(copyusers));
+        toast.warn("User deleted successfully!");
+    };
 
-   }
+    return (
+        <ol className="flex justify-start flex-col mt-5 w-1/2 m-auto container">
+            <h1 className="text-2xl mb-4">Registered Users</h1>
 
-   const mycss=
-   {
-    fontSize:"22px",
-    fontWeight:500,
-   }
-  return (
-   <>
-   <ol className='flex justify-start flex-col mt-5 w-1/2 m-auto container'>
-    <h1 className='text-2xl mb-4'>Registered Users</h1>
-    { users.length >0
-    ?users
-          .map((user,index)=>{
-            return(
-              <li key={index} className='flex justify-between mb-4 text-1xl p-4 bg-indigo-100'>
-                <Link className='hover:text-blue-400 ' to={`/show/details/${user.username }`} style={mycss}>{user.username}</Link>
-                <p>
-                  <span className='mr-6'>✏️</span>
-                  <span onClick={()=>DeleteHandler(index)
-                  }>❌</span>
-                </p>
+            {users.length > 0
+                ? users
+                      .map((user, index) => {
+                          return (
+                              <li
+                                  key={index}
+                                  className="flex justify-between mb-4 text-1xl p-4 bg-indigo-100"
+                              >
+                                  {user.username}
 
-              </li>
-            );
-          })
-          .reverse()
-        :"Loading ..."  
-    }
-    </ol>
-    <hr  className='w-[80%] m-auto my-5'/>
-    <div className='w-[80%]'>
-      <Outlet/>
-    </div>
-   
-   </>
-  )
+                                  <p>
+                                      <span className="mr-6">✏️</span>
+                                      <span
+                                          onClick={() => DeleteHandler(index)}
+                                      >
+                                          ❌
+                                      </span>
+                                  </p>
+                              </li>
+                          );
+                      })
+                      .reverse()
+                : "Loading..."}
+        </ol>
+    );
 };
 
-export default Show
+export default Show;
